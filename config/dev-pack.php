e15ba1582c21483d07d0466ab47c4dcfc0f72df3
Sacha Corazzi <sacha.corazzi@gmail.com>
2017-05-26 21:53:18 +0100

    Add default config file

---
+++ config/dev-pack.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /**
+     * These packages and their respective service providers
+     * will be loaded into your app unless you set their value to false.
+     */
+    'enabled' => [
+        'barryvdh/laravel-debugbar'      => true,
+        'barryvdh/laravel-ide-helper'    => true,
+        'filp/whoops'                    => true,
+        'laracademy/interactive-make'    => true,
+        'laravel/tinker'                 => true,
+        'rap2hpoutre/laravel-log-viewer' => true,
+        'themsaid/laravel-mail-preview'  => true,
+    ],
+];
